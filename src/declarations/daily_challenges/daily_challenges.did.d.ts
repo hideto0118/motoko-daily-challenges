cd33ccf3ad3af1b48a121c07fa7855158032cea5
Hideto Yasunori <hideto0118yasunori@gmail.com>
2022-03-08 00:26:57 -0800

    Done challnege 2 day 2

--- src/declarations/daily_challenges/daily_challenges.did.d.ts
+++ src/declarations/daily_challenges/daily_challenges.did.d.ts
@@ -1,4 +1,5 @@
 import type { Principal } from '@dfinity/principal';
 export interface _SERVICE {
-  'add2' : (arg_0: bigint, arg_1: bigint) => Promise<bigint>,
+  'max_number_with_n_bits' : (arg_0: bigint) => Promise<bigint>,
+  'nat_to_nat8' : (arg_0: bigint) => Promise<number>,
 }
--- src/declarations/daily_challenges/daily_challenges.did.js
+++ src/declarations/daily_challenges/daily_challenges.did.js
@@ -1,4 +1,7 @@
 export const idlFactory = ({ IDL }) => {
-  return IDL.Service({ 'add2' : IDL.Func([IDL.Nat, IDL.Nat], [IDL.Nat], []) });
+  return IDL.Service({
+    'max_number_with_n_bits' : IDL.Func([IDL.Nat], [IDL.Nat], []),
+    'nat_to_nat8' : IDL.Func([IDL.Nat], [IDL.Nat8], []),
+  });
 };
 export const init = ({ IDL }) => { return []; };
